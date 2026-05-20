90b7a74aad53fa776a15e14e98a915442a646f21
Ali Sharjeel <ali.sharjeel2014@gmail.com>
2026-05-20 06:22:57 +0500

    feat: add postinstall welcome script with ASCII art logo by Ali Sharjeel

---
+++ scripts/welcome.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const art = `
+\x1b[36m\x1b[1m    ___                         __  ____  ___  
+   /   |  ____ ____  ____  / /_/ __ \\/   | 
+  / /| | / __ \`/ _ \\/ __ \\/ __/ / / / /| | 
+ / ___ |/ /_/ /  __/ / / / /_/ /_/ / ___ | 
+/_/  |_|\\__, /\\___/_/ /_/\\__/\\___\\_/_/  |_|
+       /____/                              \x1b[0m
+`;
+
+console.log(art);
+console.log("\x1b[32m\x1b[1m🚀 AgentQA successfully installed!\x1b[0m");
+console.log("\x1b[37m🤖 AI-powered QA automation for coding agents & CI/CD\x1b[0m");
+console.log("\x1b[35m✨ Created by Ali Sharjeel\x1b[0m");
+console.log("");
+console.log("\x1b[33mGet started with:\x1b[0m");
+console.log("  \x1b[36magentqa config\x1b[0m             Configure API keys & vision mode");
+console.log("  \x1b[36magentqa <url> <prompt>\x1b[0m     Run a QA task");
+console.log("  \x1b[36magentqa app\x1b[0m                Launch the Electron Desktop GUI");
+console.log("");
+console.log("\x1b[90mDocumentation & info: https://github.com/AliSharjeell/AgentQA\x1b[0m");
+console.log("");
